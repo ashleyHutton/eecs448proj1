1143b64405d752da853a62573aab5e0845e11bda
Jinuk Park <kebiya93@gmail.com>
2016-02-08 15:25:53 -0600

    Fix typo error

--- Time.java
+++ Time.java
@@ -1,27 +1,29 @@
-private int hour;
-private int minute;
-private int second;
+public class Time {
+	private int hour;
+	private int minute;
+	private int second;
 
-public int getHour() {
-	return hour;
-}
+	public int getHour() {
+		return hour;
+	}
 
-public void setHour(int hour) {
-	this.hour = hour;
-}
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
 
-public int getMinute() {
-	return mintue;
-}
+	public int getMinute() {
+		return minute;
+	}
 
-public void setMinute(int minute) {
-	this.minute = minute;
-}
+	public void setMinute(int minute) {
+		this.minute = minute;
+	}
 
-public int getsecond() {
-	return second;
-}
+	public int getsecond() {
+		return second;
+	}
 
-public void setsecond(int second) {
-	this.second = second;
+	public void setsecond(int second) {
+		this.second = second;
+	}
 }
