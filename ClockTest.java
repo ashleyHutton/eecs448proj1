35689117f172e9c0797a721a5f371e402c2bc400
Francinaldo Almeida <fap_sb@globomail.com>
2016-02-08 15:30:09 -0600

    Add printMenu method

--- ClockTest.java
+++ ClockTest.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 
 public class ClockTest{
 	
@@ -9,6 +9,7 @@ public class ClockTest{
 		
 		while(true){
 			System.out.print("\033[H\033[2J");
+			printMenu();
 			System.out.println(i);
 			try{
 				Thread.sleep(1000);
@@ -23,6 +24,15 @@ public class ClockTest{
 		
 		
 		
+	}
+	
+	protected static void printMenu(){
+		System.out.println("Clock Menu:\n");
+		System.out.println("Type \"a\" to set the time");
+		System.out.println("Type \"b\" to set the format to 12 hours");
+		System.out.println("Type \"c\" to set the format to 24 hours");
+		System.out.println("Type \"d\" to launch\n");
+		System.out.println("Select one option:");
 	}
 	
 	
