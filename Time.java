07549c171d9457ad313b3b42e87c74b3b3fada3a
Jinuk Park <kebiya93@gmail.com>
2016-02-08 15:36:26 -0600

    Add constructors

--- Time.java
+++ Time.java
@@ -2,7 +2,21 @@ public class Time {
 	private int hour;
 	private int minute;
 	private int second;
+	
+	public time()
+	{
+		hour = 0;
+		minute = 0;
+		second = 0;
+	}
 
+	public time(int hour, int minute, int second)
+	{
+		this.hour = hour;
+		this.minute = minute;
+		this.second = second;
+	}
+	
 	public int getHour() {
 		return hour;
 	}
