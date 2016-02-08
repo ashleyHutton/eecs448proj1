7fbe5dc09b23eb3949d8a7f4f0d02ff87def4197
Jinuk Park <kebiya93@gmail.com>
2016-02-08 15:16:30 -0600

    Add variables, mutators and accessors

---
+++ Time.java
@@ -0,0 +1,27 @@
+private int hour;
+private int minute;
+private int second;
+
+public int getHour() {
+	return hour;
+}
+
+public void setHour(int hour) {
+	this.hour = hour;
+}
+
+public int getMinute() {
+	return mintue;
+}
+
+public void setMinute(int minute) {
+	this.minute = minute;
+}
+
+public int getsecond() {
+	return second;
+}
+
+public void setsecond(int second) {
+	this.second = second;
+}
