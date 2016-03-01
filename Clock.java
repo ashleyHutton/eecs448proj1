2a93da9ee5a3fa4e42e751c33f61bb07c31c3da5
Hannah L. Johnson <hlj.inbox@gmail.com>
2016-02-29 19:32:10 -0600

    Add comments to gui function.

--- Clock.java
+++ Clock.java
@@ -23,14 +23,17 @@ public class Clock{
 
 	public void gui(){
 
+		// Create window
 		f = new JFrame("Clock");
 		f.setVisible(true);
 		f.setSize(600,400);
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // program stops when x clicked
 
+		// Create panel and make background white
 		p = new JPanel();
 		p.setBackground(Color.WHITE);
 
+		// Create button and button action
 		b1 = new JButton("Start Clock");
 		b1.addActionListener(new ActionListener(){
 
@@ -42,10 +45,12 @@ public class Clock{
 
 		});
 
+		// add button to panel
 		p.add(b1);
 
+		// add panel to the frame
 		f.add(p);
-		
+
 	}
 
 
@@ -55,4 +60,4 @@ public class Clock{
 
 	}
 
-}	
+}
